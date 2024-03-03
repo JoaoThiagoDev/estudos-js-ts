6a878f3e6d14481cd562952398d6173210d71d6a
joaothiagodev <joaothiagossouza2004@gmail.com>
2024-03-02 22:09:34 -0300

    Sistema de login e register

--- Sec 10 - Node.js/projeto-agenda/src/controllers/loginController.js
+++ Sec 10 - Node.js/projeto-agenda/src/controllers/loginController.js
@@ -1,8 +1,17 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-    res.render('login');
+    if (!req.session.user) {
+        return res.render('login');
+    }
+    return res.render('login-logged');
 };
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect('/');
+}
+
 
 exports.register = async (req, res) => {
     try {
@@ -22,7 +31,39 @@ exports.register = async (req, res) => {
 
         req.flash('success', 'Usuário cadastrado com sucesso!');
         req.session.save(function () {
-            return res.redirect('/login/index');
+            return res.redirect('/');
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.render('404');
+    }
+}
+
+
+
+exports.login = async (req, res) => {
+    try {
+
+        const login = new Login(req.body);
+
+        await login.login();
+
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema com sucesso.');
+
+        req.session.user = login.user;
+
+        req.session.save(function () {
+            return res.redirect('/');
         });
 
     } catch (error) {
--- Sec 10 - Node.js/projeto-agenda/src/models/LoginModel.js
+++ Sec 10 - Node.js/projeto-agenda/src/models/LoginModel.js
@@ -13,10 +13,34 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
     constructor(body) {
-        this.body = body;
+        this.body = body;  // de onde vêm email/user e password
         this.errors = [];
         this.user = null;
     }
+
+
+
+
+    async login() {
+        this.valida()
+        if (this.errors.length > 0) return;
+
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if (!this.user) {
+            this.errors.push('Usuário não cadastrado.');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha incorreta!');
+            this.user = null;
+            return;
+        }
+
+
+    }
+
 
     async register() {
         this.valida();
@@ -27,41 +51,38 @@ class Login {
 
 
         if (this.errors.length > 0) return;
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try {
-            const salt = bcryptjs.genSaltSync();
-            this.body.password = bcryptjs.hashSync(this.body.password, salt);
-            this.user = await LoginModel.create(this.body) // CRIA UM NOVO OBJETO CASO NAO TIVER ERRO
-        }
-        catch (e) {
-            console.log(e);
-        }
+        this.user = await LoginModel.create(this.body);
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({
-            email: this.body.email,
-        })
+        const user = await LoginModel.findOne({ email: this.body.email });
 
         if (user) this.errors.push('Usuário já está cadastrado.');
     }
 
-    valida() {
+    async valida() {
         this.cleanUp();
         // Validação:
         /*
-            - Email precisa ser válido
-            - Senha entre 6 e 50 caracteres
+        - Email precisa ser válido
+        - Senha entre 6 e 50 caracteres
         */
 
         // Validando EMAIL
         if (!validator.isEmail(this.body.email)) {
             this.errors.push('E-mail inválido');
+        } else {
+            const user = await LoginModel.findOne({ email: this.body.email });
+            if (!user) {
+                if (this.body.password.length < 6 || this.body.password.length > 50) {
+                    this.errors.push('A senha precisa ter entre 6 e 50 caracteres.');
+                }
+            }
         }
 
-        if (this.body.password.length < 6 || this.body.password.length > 50) {
-            this.errors.push('A senha precisa ter entre 6 e 50 caracteres.');
-        }
     }
 
     cleanUp() {
